bb126e451e1996fa4a411a715d9b9d3e9878eb10
lilian131 <bideaulilian@gmail.com>
2017-11-18 08:09:09 +0300

    optimisation du code javascript pour plus de gestion des erreurs

--- calcul.js
+++ calcul.js
@@ -6,20 +6,33 @@ var res = document.querySelector('#resultat');
 button.addEventListener('click', function(e){
 	e.preventDefault();
 	var ope = operateur.value;
+	var result;
 	switch(ope) {
-		case 1:
+		case '1':
 			ope = '+';
 			break;
-		case 2:
+		case '2':
 			ope = '*';
 			break;
-		case 3:
+		case '3':
 			ope = '-';
 			break;
-		case 4:
+		case '4':
 			ope = '/';
 			break;
 	}
-	var result = input1.value + ope + input2.value;
-	res.innerHTML = eval(result);
+
+	if (input1.value === '' || input2.value === '' || ope === '') {
+		result = 'aucun champ ne doit etre vide';
+		res.innerHTML = result;
+		return;
+	} else if (ope == '/' && input1.value === '0' || input1.value === '0') {
+		result = 'la division par 0 est impossible';
+		res.innerHTML = result;
+		return;
+	} 
+		result = input1.value + ope + input2.value;
+		res.innerHTML = eval(result)
+	
+	
 })
